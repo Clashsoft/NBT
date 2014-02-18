773b5ae82ad4cd8c833129b3aaf206d535723be0
Clashsoft <clash2@hotmail.de>
2014-02-18 12:26:49 +0100

    writeArray and readArray IO methods

--- src/com/clashsoft/nbt/io/NBTInputStream.java
+++ src/com/clashsoft/nbt/io/NBTInputStream.java
@@ -53,4 +53,135 @@ public class NBTInputStream extends DataInputStream
 			return String.valueOf(chars);
 		}
 	}
+	
+	public boolean[] readBooleanArray() throws IOException
+	{
+		int len = this.readInt();
+		byte[] data = this.readByteArray();
+		
+		boolean[] bools = new boolean[len];
+		for (int i = 0, j = 0, k = 7; i < len; i++)
+		{
+			if ((data[j] & (1 << k--)) == 1)
+			{
+				bools[i] = true;
+			}
+			
+			if (k < 0)
+			{
+				j++;
+				k = 7;
+			}
+		}
+		
+		return bools;
+	}
+	
+	public byte[] readByteArray() throws IOException
+	{
+		int len = this.readInt();
+		byte[] v = new byte[len];
+		
+		for (int i = 0; i < len; i++)
+		{
+			v[i] = this.readByte();
+		}
+		return v;
+	}
+	
+	public short[] readShortArray() throws IOException
+	{
+		int len = this.readInt();
+		short[] v = new short[len];
+		
+		for (int i = 0; i < len; i++)
+		{
+			v[i] = this.readShort();
+		}
+		return v;
+	}
+	
+	public char[] readCharArray() throws IOException
+	{
+		int len = this.readInt();
+		char[] v = new char[len];
+		
+		for (int i = 0; i < len; i++)
+		{
+			v[i] = this.readChar();
+		}
+		return v;
+	}
+	
+	public int[] readMediumArray() throws IOException
+	{
+		int len = this.readInt();
+		int[] v = new int[len];
+		
+		for (int i = 0; i < len; i++)
+		{
+			v[i] = this.readMedium();
+		}
+		return v;
+	}
+	
+	public int[] readIntArray() throws IOException
+	{
+		int len = this.readInt();
+		int[] v = new int[len];
+		
+		for (int i = 0; i < len; i++)
+		{
+			v[i] = this.readInt();
+		}
+		return v;
+	}
+	
+	public long[] readLongArray() throws IOException
+	{
+		int len = this.readInt();
+		long[] v = new long[len];
+		
+		for (int i = 0; i < len; i++)
+		{
+			v[i] = this.readLong();
+		}
+		return v;
+	}
+	
+	public float[] readFloatArray() throws IOException
+	{
+		int len = this.readInt();
+		float[] v = new float[len];
+		
+		for (int i = 0; i < len; i++)
+		{
+			v[i] = this.readFloat();
+		}
+		return v;
+	}
+	
+	public double[] readDoubleArray() throws IOException
+	{
+		int len = this.readInt();
+		double[] v = new double[len];
+		
+		for (int i = 0; i < len; i++)
+		{
+			v[i] = this.readDouble();
+		}
+		return v;
+	}
+	
+	public String[] readStringArray() throws IOException
+	{
+		int len = this.readInt();
+		String[] v = new String[len];
+		
+		for (int i = 0; i < len; i++)
+		{
+			v[i] = this.readString();
+		}
+		return v;
+	}
 }
--- src/com/clashsoft/nbt/io/NBTOutputStream.java
+++ src/com/clashsoft/nbt/io/NBTOutputStream.java
@@ -24,13 +24,14 @@ public class NBTOutputStream extends DataOutputStream
 	}
 	
 	/**
-	 * Increases the written counter by the specified value
-	 * until it reaches Integer.MAX_VALUE.
+	 * Increases the written counter by the specified value until it reaches
+	 * Integer.MAX_VALUE.
 	 */
 	public void incCount(int value)
 	{
 		int temp = written + value;
-		if (temp < 0) {
+		if (temp < 0)
+		{
 			temp = Integer.MAX_VALUE;
 		}
 		written = temp;
@@ -61,6 +62,162 @@ public class NBTOutputStream extends DataOutputStream
 			{
 				this.writeChar(str.charAt(i));
 			}
+		}
+	}
+	
+	public void writeBooleanArray(boolean[] v) throws IOException
+	{
+		int len = v.length;
+		this.writeInt(len);
+		
+		int len1 = (len / 8) + ((len & 7) != 0 ? 1 : 0);
+		byte[] data = new byte[len1];
+		
+		for (int i = 0, j = 0, k = 7; i < len; i++)
+		{
+			if (v[i])
+			{
+				data[j] |= 1 << k--;
+			}
+			
+			if (k < 0)
+			{
+				j++;
+				k = 7;
+			}
+		}
+		
+		this.writeByteArray(data);
+	}
+	
+	public void writeByteArray(byte[] v) throws IOException
+	{
+		int len = v.length;
+		this.writeInt(len);
+		for (int i = 0; i < len; i++)
+		{
+			byte b = v[i];
+			out.write(b);
+		}
+		incCount(len);
+	}
+	
+	public void writeShortArray(short[] v) throws IOException
+	{
+		int len = v.length;
+		this.writeInt(len);
+		for (int i = 0; i < len; i++)
+		{
+			short s = v[i];
+			out.write((s >>> 8) & 0xFF);
+			out.write((s >>> 0) & 0xFF);
+		}
+		incCount(len * 2);
+	}
+	
+	public void writeCharArray(char[] v) throws IOException
+	{
+		int len = v.length;
+		this.writeInt(len);
+		for (int i = 0; i < len; i++)
+		{
+			int c = v[i];
+			out.write((c >>> 8) & 0xFF);
+			out.write((c >>> 0) & 0xFF);
+		}
+		incCount(len * 2);
+	}
+	
+	public void writeMediumArray(int[] v) throws IOException
+	{
+		int len = v.length;
+		this.writeInt(len);
+		for (int i = 0; i < len; i++)
+		{
+			int m = v[i];
+			out.write((m >>> 16) & 0xFF);
+			out.write((m >>> 8) & 0xFF);
+			out.write((m >>> 0) & 0xFF);
+		}
+		incCount(len * 3);
+	}
+	
+	public void writeIntArray(int[] v) throws IOException
+	{
+		int len = v.length;
+		this.writeInt(len);
+		for (int i = 0; i < len; i++)
+		{
+			int j = v[i];
+			out.write((j >>> 24) & 0xFF);
+			out.write((j >>> 16) & 0xFF);
+			out.write((j >>> 8) & 0xFF);
+			out.write((j >>> 0) & 0xFF);
+		}
+		incCount(len * 4);
+	}
+	
+	public void writeLongArray(long[] v) throws IOException
+	{
+		int len = v.length;
+		this.writeInt(len);
+		for (int i = 0; i < len; i++)
+		{
+			long l = v[i];
+			out.write((int) ((l >>> 56L) & 0xFF));
+			out.write((int) ((l >>> 48L) & 0xFF));
+			out.write((int) ((l >>> 40) & 0xFF));
+			out.write((int) ((l >>> 32L) & 0xFF));
+			out.write((int) ((l >>> 24L) & 0xFF));
+			out.write((int) ((l >>> 16L) & 0xFF));
+			out.write((int) ((l >>> 8L) & 0xFF));
+			out.write((int) ((l >>> 0L) & 0xFF));
+		}
+		incCount(len * 8);
+	}
+	
+	public void writeFloatArray(float[] v) throws IOException
+	{
+		int len = v.length;
+		this.writeInt(len);
+		for (int i = 0; i < len; i++)
+		{
+			int j = Float.floatToIntBits(v[i]);
+			out.write((j >>> 24) & 0xFF);
+			out.write((j >>> 16) & 0xFF);
+			out.write((j >>> 8) & 0xFF);
+			out.write((j >>> 0) & 0xFF);
+		}
+		incCount(len * 4);
+	}
+	
+	public void writeDoubleArray(double[] v) throws IOException
+	{
+		int len = v.length;
+		this.writeInt(len);
+		for (int i = 0; i < len; i++)
+		{
+			long l = Double.doubleToLongBits(v[i]);
+			out.write((int) ((l >>> 56L) & 0xFF));
+			out.write((int) ((l >>> 48L) & 0xFF));
+			out.write((int) ((l >>> 40) & 0xFF));
+			out.write((int) ((l >>> 32L) & 0xFF));
+			out.write((int) ((l >>> 24L) & 0xFF));
+			out.write((int) ((l >>> 16L) & 0xFF));
+			out.write((int) ((l >>> 8L) & 0xFF));
+			out.write((int) ((l >>> 0L) & 0xFF));
+		}
+		incCount(len * 8);
+	}
+	
+	public void writeStringArray(String[] v) throws IOException
+	{
+		int len = v.length;
+		this.writeInt(len);
+		for (int i = 0; i < len; i++)
+		{
+			String s = v[i];
+			this.writeString(s);
 		}
 	}
 }
